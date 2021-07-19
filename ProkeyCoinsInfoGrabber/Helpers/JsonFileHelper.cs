7b7c894083bd69ac1810e2b1a45bf643213eb459
kohansal6396 <mahmoud.kohansal@gmail.com>
2021-07-19 16:35:34 +0430

    JsonFileHelper(2)

--- ProkeyCoinsInfoGrabber/Helpers/JsonFileHelper.cs
+++ ProkeyCoinsInfoGrabber/Helpers/JsonFileHelper.cs
@@ -52,5 +52,34 @@ namespace ProkeyCoinsInfoGrabber.Helpers
             }
 
         }
+
+        public static T DeserializeFile(string filePath)
+        {
+            if(!File.Exists(filePath))
+            {
+                ConsoleUtiliy.LogError($"Json file not found!: {filePath}");
+                return default;
+            }
+            try
+            {
+                string fileContent = File.ReadAllText(filePath);
+                return JsonSerializer.Deserialize<T>(fileContent);
+            }
+            catch (IOException ioExp)
+            {
+                ConsoleUtiliy.LogError($"IOException in {filePath}: {ioExp.Message}");
+                return default;
+            }
+            catch (JsonException jsonExp)
+            {
+                ConsoleUtiliy.LogError($"JsonException in {filePath}: {jsonExp.Message}");
+                return default;
+            }
+            catch (Exception exp)
+            {
+                ConsoleUtiliy.LogError($"Exception in {filePath}: {exp.Message}");
+                return default;
+            }
+        }
     }
 }

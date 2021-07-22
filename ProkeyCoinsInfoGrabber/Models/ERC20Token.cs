8d97b30634dca8aaf727dda588cf7c15138e5aa4
Hadi <hadi.robati@gmail.com>
2021-07-22 12:46:59 +0800

    Update ERC20Token.cs

--- ProkeyCoinsInfoGrabber/Models/ERC20Token.cs
+++ ProkeyCoinsInfoGrabber/Models/ERC20Token.cs
@@ -11,27 +11,20 @@ namespace ProkeyCoinsInfoGrabber.Models
         public string id { get; set; }
         public string symbol { get; set; } = string.Empty;
 
-        //"address": "0x0000000000b3F879cb30FE243b4Dfee438691c04",
         public string address { get; set; } = string.Empty;
 
-        //"decimals": 2,
         public int decimals { get; set; }
 
         public string name { get; set; } = string.Empty;
 
-        //"ens_address": "",
         public string ens_address { get; set; } = string.Empty;
 
-        //"website": "",
         public string website { get; set; } = string.Empty;
 
-        // "logo": {
         public ERC20TokenLogo logo { get; set; } = new ERC20TokenLogo();
 
-        //"support": {
         public ERC20TokenSupport support { get; set; } = new ERC20TokenSupport();
 
-        //"social": {
         public ERC20TokenSocial social { get; set; } = new ERC20TokenSocial();
 
         public void Map(EthplorerGetTokenInfoApiResponse tokenInfo)
@@ -92,12 +85,6 @@ namespace ProkeyCoinsInfoGrabber.Models
     }
     class ERC20TokenLogo
     {
-        //    "logo": {
-        //  "src": "",
-        //  "width": "",
-        //  "height": "",
-        //  "ipfs_hash": ""
-        //}
         public string src { get; set; } = string.Empty;
         public string width { get; set; } = string.Empty;
         public string height { get; set; } = string.Empty;
@@ -105,10 +92,6 @@ namespace ProkeyCoinsInfoGrabber.Models
     }
     class ERC20TokenSupport
     {
-        //  "support": {
-        //"email": "",
-        //"url": ""
-        //  },
         [EmailAddress]
         public string email { get; set; } = string.Empty;
         public string url { get; set; } = string.Empty;
@@ -116,85 +99,33 @@ namespace ProkeyCoinsInfoGrabber.Models
     
     class ERC20TokenSocial
     {
-        //    "social": {
-        //  "blog": "",
         public string blog { get; set; } = string.Empty;
 
-        //  "chat": "",
         public string chat { get; set; } = string.Empty;
 
-        //  "discord": "",
         public string discord { get; set; } = string.Empty;
 
-        //  "facebook": "",
         public string facebook { get; set; } = string.Empty;
 
-        //  "forum": "",
         public string forum { get; set; } = string.Empty;
 
-        //  "github": "",
         public string github { get; set; } = string.Empty;
 
-        //  "gitter": "",
         public string gitter { get; set; } = string.Empty;
 
-        //  "instagram": "",
         public string instagram { get; set; } = string.Empty;
 
-        //  "linkedin": "",
         public string linkedin { get; set; } = string.Empty;
 
-        //  "reddit": "",
         public string reddit { get; set; } = string.Empty;
 
-        //  "slack": "",
         public string slack { get; set; } = string.Empty;
 
-        //  "telegram": "",
         public string telegram { get; set; } = string.Empty;
 
-        //  "twitter": "",
         public string twitter { get; set; } = string.Empty;
 
-        //  "youtube": ""
         public string youtube { get; set; } = string.Empty;
 
-        //}
     }
 }
-/*
- {
-    "symbol": "GST2",
-    "address": "0x0000000000b3F879cb30FE243b4Dfee438691c04",
-    "decimals": 2,
-    "name": "Gastoken.io",
-    "ens_address": "",
-    "website": "",
-    "logo": {
-      "src": "",
-      "width": "",
-      "height": "",
-      "ipfs_hash": ""
-    },
-    "support": {
-      "email": "",
-      "url": ""
-    },
-    "social": {
-      "blog": "",
-      "chat": "",
-      "discord": "",
-      "facebook": "",
-      "forum": "",
-      "github": "",
-      "gitter": "",
-      "instagram": "",
-      "linkedin": "",
-      "reddit": "",
-      "slack": "",
-      "telegram": "",
-      "twitter": "",
-      "youtube": ""
-    }
-}
- */
